2ccf2aa2e102d479142df27e0c007a0ac2b94cd6
rodrigo00737 <rodrigo00737@gmail.com>
2020-02-09 17:56:36 -0300

    commit notebook

--- TemplateGentelella/Models/PacienteClinica.cs
+++ TemplateGentelella/Models/PacienteClinica.cs
@@ -7,5 +7,11 @@ namespace TemplateGentelella.Models
 {
     public class PacienteClinica
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
     }
 }
---
+++ TemplateGentelella/Models/PacienteClinicaPlanoSaude.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TemplateGentelella.Models
+{
+    public class PacienteClinicaPlanoSaude
+    {
+        public int Id { get; set; }
+        public int PacienteClinicaId { get; set; }
+        public string PlanoSaude { get; set; }
+        public float ValorLiberado { get; set; }
+    }
+}
